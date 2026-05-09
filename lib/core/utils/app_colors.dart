ba128493d602d72c404c66818cf14438df98d67a
Sawant-Diksha <sawantdiksha511@gmail.com>
2026-05-09 23:11:08 +0530

    added some folders and color and login file added

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF6A5AE0);
+  static const background = Color(0xFFF3F0FD);
+  static const textDark = Color(0xFF000000);
+  static const textGrey = Color(0xFF6B7280);
+  static const textFaintGrey = Color(0xFF9CA3AF);
+  static const circle1 = Color(0xFF7E3BED);
+  static const circle2 = Color(0xFFAAA4F7);
+  static const white = Color(0xFFFFFFFF);
+}
